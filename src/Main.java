79fcdbe3309fbd1853c08716aee757a79c3236e0
Faroukii <122316189+Faroukii@users.noreply.github.com>
2023-02-12 18:20:37 -0500

    Lab_05_02_BirthMonth

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int birthMonth = 13;
+        if (birthMonth >= 1) ;
+        if (birthMonth <= 12) ;
+        if (birthMonth <= 1) ;
+        if (birthMonth >= 12) ;
+        System.out.println("Your birth month is: " + birthMonth);
+        System.out.println("You have entered an incorrect month value: " + birthMonth);
+
+    }
+
+
+
+
+    }
+
+
+
